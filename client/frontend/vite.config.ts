cd1c86354abfbb8828690455ae7599a72aa7828b
chrisPfaff <chrispfaff10@gmail.com>
2024-05-06 18:35:01 -0500

    Initial commit

---
+++ client/frontend/vite.config.ts
@@ -0,0 +1,37 @@
+import { URL, fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+
+import react from "@vitejs/plugin-react";
+
+export default defineConfig((env) => {
+  const envars = loadEnv(env.mode, "./");
+
+  const serverURL = new URL(
+    envars.VITE_SERVER_URL ?? "<http://localhost:3001>"
+  );
+  const serverAPIPath = envars.VITE_SERVER_API_PATH ?? "/api";
+
+  return {
+    envDir: "./",
+
+    define: {
+      __API_PATH__: JSON.stringify(serverAPIPath),
+    },
+
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+
+    server: {
+      port: 5173,
+      proxy: {
+        // proxy requests with the API path to the server
+        // <http://localhost:5173/api> -> <http://localhost:3001/api>
+        [serverAPIPath]: serverURL.origin,
+      },
+    },
+  };
+});
