35fd69ebe07f8d088a6553fe87e639671a8574a8
chrisPfaff <chrispfaff10@gmail.com>
2024-05-07 13:02:49 -0500

    docker contained

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:21
+
+WORKDIR /server
+
+COPY server/package*.json server/yarn.lock ./
+RUN yarn install
+# Install dependencies
+COPY server/ .
+
+WORKDIR /client
+
+COPY client/frontend/package*.json client/frontend/yarn.lock ./
+RUN yarn install
+COPY client/ .
+
+WORKDIR /server
+ENV port=3001
+EXPOSE 3001
+CMD ["yarn", "dev"]
+
+
+
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,14 +2,19 @@ import "dotenv/config";
 
 import cors from "cors";
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "../../client/frontend/dist")));
 const PORT = process.env.PORT || 3001;
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/frontend/dist/index.html"));
+});
 
 app.get("/api", (_req, res) => {
   res.status(200).json({ message: "Hello from the server!" });
